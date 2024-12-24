677d1d8e4f59353a25c2eeeb9c8c77250ebc6202
oliver194 <100048880+oliver194@users.noreply.github.com>
2024-12-24 12:41:25 +0000

    Added CPU info and updated bstats.

--- src/main/java/me/oliver193/basicplugin/commands/ServerInfoCommand.java
+++ src/main/java/me/oliver193/basicplugin/commands/ServerInfoCommand.java
@@ -5,6 +5,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
 
 import java.lang.management.ManagementFactory;
 
@@ -14,8 +17,13 @@ public class ServerInfoCommand implements CommandExecutor {
         System.gc();
         Runtime rt = Runtime.getRuntime();
         long usedMB = (rt.totalMemory() - rt.freeMemory()) / 1024 / 1024;
-        long totalMB = (rt.totalMemory()) / 1024 / 1024;
         long maxMB = (rt.maxMemory()) / 1024 / 1024;
+
+        // Get processor information
+        SystemInfo systemInfo = new SystemInfo();
+        HardwareAbstractionLayer hardware = systemInfo.getHardware();
+        CentralProcessor processor = hardware.getProcessor();
+        CentralProcessor.ProcessorIdentifier processorIdentifier = processor.getProcessorIdentifier();
 
 
         // Get uptime in milliseconds
@@ -32,6 +40,11 @@ public class ServerInfoCommand implements CommandExecutor {
         Msg.send(commandSender, "&7------------= &aServer Info &7=------------");
         Msg.send(commandSender, "Server Version: " + Bukkit.getBukkitVersion());
         Msg.send(commandSender, "Software: " + Bukkit.getName());
+        Msg.send(commandSender, "Java Version: " + System.getProperty("java.version"));
+        Msg.send(commandSender, "OS: " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ") " + System.getProperty("os.arch"));
+        Msg.send(commandSender, "CPU: " + processorIdentifier.getName());
+        Msg.send(commandSender, "CPU Speed: " + processorIdentifier.getVendorFreq() / 1000000000.0 + " GHz");
+        Msg.send(commandSender, "Core Count: " + processor.getPhysicalProcessorCount() + " cores / " + processor.getLogicalProcessorCount() + " threads");
         Msg.send(commandSender, "RAM Usage: " + usedMB + " MB" + " of " + maxMB + " MB");
         Msg.send(commandSender, "Uptime: " + minutes + " minutes and " + seconds + " seconds");
 
